d872b8388b48e6f11d76a4a7a85cde17e8fd12cb
JosieD <jgseumo@gmail.com>
2025-11-01 23:34:01 -0500

    compute total, income and expense and pass them as props to the other components

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,43 @@ import Balance from './components/Balance.vue';
 import IncomeExpense from './components/IncomeExpense.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
-import { ref } from 'vue'; //make it reactive
+import { ref, computed } from 'vue';
+
 
 const transactions = ref([
       { id: 1, text: 'Cash', amount: -400 },
       { id: 2, text: 'Paycheck', amount: 800 },
       { id: 3, text: 'Flower', amount: -19.99 },
       { id: 4, text: 'Camera', amount: 150.99 },
+      { id: 5, text: 'Bread', amount: -5.99 },
     ]);
+
+//Get total amount
+const total = computed(() => {
+    return transactions.value.reduce((acc, transaction) => acc + transaction.amount, 0);
+});
+
+//Get income
+const income = computed(() => {
+    return transactions.value
+        .filter(transaction => transaction.amount > 0)
+        .reduce((acc, transaction) => acc + transaction.amount, 0).toFixed(2);
+});
+
+//Get expense
+const expense = computed(() => {
+    return transactions.value
+        .filter(transaction => transaction.amount < 0)
+        .reduce((acc, transaction) => acc + transaction.amount, 0).toFixed(2);
+});
 </script>
 
 <template>
     <Header />
     <div class="container">
-        <Balance />
-        <IncomeExpense />
-        <TransactionList :transactions="transactions" /> <!-- pass transactions as prop -->
+        <Balance :total/>
+        <IncomeExpense :income :expense/>
+        <TransactionList :transactions="transactions" />
         <AddTransaction />
     </div>
 </template>
